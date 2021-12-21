64cff32e8ab7c9317de3fefd2905714912e80aee
unknown <ksm1538@naver.com>
2021-12-21 23:02:40 +0900

    react_bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
+import { Navbar,Nav,NavDropdown,Form,Button,FormControl,Container,Carousel } from 'react-bootstrap';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+          <Navbar bg="dark" variant="dark">
+            <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+            </Container>
+          </Navbar>
+
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-80"
+                src="./shoes1.jpg"
+                alt="1번째 신발"
+              />
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-80"
+                src="./shoes2.jpg"
+                alt="2번째 신발"
+              />
+
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-80"
+                src="./shoes3.jpg"
+                alt="3번째 신발"
+              />
+
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
     </div>
   );
 }
