c1254884f8c79a719a02303aaa43ab6d14baa179
unknown <ksm1538@naver.com>
2021-12-24 14:53:41 +0900

    temp

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Navbar,Nav,NavDropdown,Form,Button,FormControl,Container,Carousel } fro
 import React, { useState } from 'react';
 import './App.css';
 import Data from './data.js'
+import { Link, Route, Switch } from 'react-router-dom';
 
 function App() {
 
@@ -20,29 +21,44 @@ function App() {
             </Container>
           </Navbar>
 
-          <img
-            src="./appre.png"
-            alt="thank_you"
-            style={ {align:"center", width:"30%"} }
-          />
-          <br/>
-          <br/>
-          <h3>오늘의 신발 세일</h3>
-          <br/>
+            <Route path="/"> 
+              <img
+                src="./appre.png"
+                alt="thank_you"
+                style={ {align:"center", width:"30%"} }
+              />
+              <br/>
+              <br/>
+              <h3>오늘의 신발 세일</h3>
+              <br/>
 
-          <div className="container">
-            <div className="row">
-              {
-                shoesData.map(function(item,cnt){
-                  return(
-                      <ShoesList data={shoesData[cnt]} key={cnt}></ShoesList>
-                  )
-                })
-              }
-              
-              
-            </div>
-          </div>
+              <div className="container">
+                <div className="row">
+                  {
+                    shoesData.map(function(item,cnt){
+                      return(
+                          <ShoesList data={shoesData[cnt]} key={cnt}></ShoesList>
+                      )
+                    })
+                  }
+                </div>
+              </div>
+            </Route>
+            <Route path="/detail">
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-6">
+                    <img src="./20211222_223425.jpg" width="100%" />
+                  </div>
+                  <div className="col-md-6 mt-4">
+                    <h4 className="pt-5">상품명</h4>
+                    <p>상품설명</p>
+                    <p>140000원</p>
+                    <button className="btn btn-danger">주문하기</button> 
+                  </div>
+                </div>
+              </div> 
+            </Route>
     </div>
   );
 }
@@ -56,4 +72,5 @@ function ShoesList(props){
       </div>
   )
 }
+
 export default App;
