d0539c50e7a59c0ea6a4663d23591fee9e2b766f
unknown <ksm1538@naver.com>
2022-01-07 21:29:58 +0900

    useSelector_and_useDispatch

--- src/basket.js
+++ src/basket.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import {Table} from 'react-bootstrap';
 import {connect} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 function Basket(props){
+  // useSelector를 이용하여 redux state 가져오는 방법
+  let state = useSelector((state) => state )
+  let dispatch = useDispatch();
+  let shoesStore = state.reducer;
+  let alertCheck = state.alertReducer;
+
   return (
     <div>
       <Table responsive>
@@ -12,25 +19,25 @@ function Basket(props){
           <th>수량</th>
           <th>변경</th>
         </tr>
-        { props.data.map((a,i)=>{
+        { shoesStore.map((a,i)=>{
           return (
           <tr key={i}>
             <td>{a.id}</td>
             <td>{a.name}</td>
             <td>{a.stock}</td>
             <td>
-                <button onClick={()=>{ props.dispatch({type: 'stockCntPlus', id:a.id}) }}> + </button> &nbsp;
-                <button onClick={()=>{ props.dispatch({type: 'stockCntMinus', id:a.id}) }}> - </button>
+                <button onClick={()=>{ dispatch({type: 'stockCntPlus', id:a.id}) }}> + </button> &nbsp;
+                <button onClick={()=>{ dispatch({type: 'stockCntMinus', id:a.id}) }}> - </button>
             </td>
           </tr>
           )
         })  }
       </Table>
       {
-          props.alertCheck == true?(
+          alertCheck == true?(
                 <div className="alert_SM">
                     <p>알림알림알림알림알림알림알림알림알림알림알림알림</p>
-                    <button onClick={()=>{ props.dispatch({type:'alertCancel'}) }}>닫기</button>
+                    <button onClick={()=>{ dispatch({type:'alertCancel'}) }}>닫기</button>
                 </div>
           ):null
       }
@@ -39,6 +46,8 @@ function Basket(props){
   )
 }
 
+/*
+connect로 redux의 store와 연결하는 방법
 function storeToProps(store){
     return {
         data : store.reducer,
@@ -47,3 +56,5 @@ function storeToProps(store){
 }
 
 export default connect(storeToProps)(Basket);
+*/
+export default Basket;
